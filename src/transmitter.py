ebbfa5571a1da457752e72af78aa03c5b717ae6c
Fernando D'Ávila <fernando.davila8@hotmail.com>
2026-03-13 18:44:19 +0100

    transmitter implementation

--- src/transmitter.py
+++ src/transmitter.py
@@ -3,6 +3,12 @@ import numpy as np
 def generate_bits(n_bits):
     return np.random.randint(0, 2, size=n_bits)
 
+def data_modulator(bits, BITS_PER_SYMBOL):
+    if BITS_PER_SYMBOL == 1:
+        symbols = 2 * bits - 1
+    else:
+        bits = bits.reshape(-1, 2)
+
 def bits2bpsk(bits):
     symbols = 2 * bits - 1
     return symbols
@@ -15,4 +21,16 @@ def bits2qpsk(bits):
 
     symbols = (I + 1j*Q) / np.sqrt(2)
     return symbols
+
+def ofdm_modulation(F_tx, CP_LEN, N_fft):
+
+    ofdm_frame = np.fft.ifft(F_tx, n=N_fft, axis=1)
+
+    CP = ofdm_frame[:, -CP_LEN:]
+    ofdm_frame_cp = np.concatenate((CP, ofdm_frame), axis=1)
+
+    tx_signal = ofdm_frame_cp.reshape(-1)
+
+    return tx_signal
+
 
