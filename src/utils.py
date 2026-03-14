850278c1b87884ba103850a0a9c682ae0f418096
Fernando D'Ávila <fernando.davila8@hotmail.com>
2026-03-14 17:09:33 +0100

    parameters + utils.py script

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import yaml
+
+C0 = 299792458.0
+KB = 1.38064852e-23
+
+def load_config(path="configs/simulation_parameters.yaml"):
+    with open(path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
