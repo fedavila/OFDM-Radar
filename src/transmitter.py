c2dbc3e3cae2ae41473473a48d552adedffa271f
Fernando D'Ávila <fernando.davila8@hotmail.com>
2026-03-14 17:09:05 +0100

    modulation func upgrade

--- src/transmitter.py
+++ src/transmitter.py
@@ -6,23 +6,18 @@ def generate_bits(n_bits):
 def data_modulator(bits, BITS_PER_SYMBOL):
     if BITS_PER_SYMBOL == 1:
         symbols = 2 * bits - 1
+
     else:
         bits = bits.reshape(-1, 2)
+        I = 2 * bits[:, 0] - 1
+        Q = 2 * bits[:, 1] - 1
+        symbols = (I + 1j*Q) / np.sqrt(2)
 
-def bits2bpsk(bits):
-    symbols = 2 * bits - 1
-    return symbols
-
-def bits2qpsk(bits):
-    bits = bits.reshape(-1, 2)
-
-    I = 2*bits[:, 0] - 1
-    Q = 2*bits[:, 1] - 1
-
-    symbols = (I + 1j*Q) / np.sqrt(2)
     return symbols
 
 def ofdm_modulation(F_tx, CP_LEN, N_fft):
+
+    # F_tx shape: (M, N)
 
     ofdm_frame = np.fft.ifft(F_tx, n=N_fft, axis=1)
 
